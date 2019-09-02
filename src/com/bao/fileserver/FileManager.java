406dbdbe95229cc3537b504171f41367c1a3bf22
baont.qn@gmail.com <baont.qn@gmail.com>
2019-09-03 00:05:54 +0700

    - implement cache for file manager, still not thread-safe

--- src/com/bao/fileserver/FileManager.java
+++ src/com/bao/fileserver/FileManager.java
@@ -3,28 +3,47 @@ package com.bao.fileserver;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+/**
+ * to make this thread-safe
+ */
 public class FileManager {
     private String folder;
+    // cache file list and content of each file to reduce disk access
+    private List<String> fileList;
+    private ConcurrentMap<String, String> contentPerFile;
 
     public FileManager(String folder) {
         this.folder = folder;
+        contentPerFile = new ConcurrentHashMap<>();
     }
 
     public List<String> listFile() throws IOException {
-        try (Stream<Path> walk = Files.walk(Paths.get(folder))) {
-            List<String> result = walk.filter(Files::isRegularFile)
-                    .map(x -> x.toString()).collect(Collectors.toList());
-            return result;
+        if (fileList == null) {
+            File folder = new File(this.folder);
+            File[] listOfFiles = folder.listFiles();
+
+            fileList = Arrays.asList(listOfFiles).stream()
+                    .filter(file -> file.isFile())
+                    .map(file -> file.getName()).collect(Collectors.toList());
+
         }
+        return fileList;
     }
 
     public String readFile(String file) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(folder + File.separator + file)));
+        // we could not use computeIfAbsent here because the lambda expression does not has
+        // IOException in its signature
+        if (!contentPerFile.containsKey(file)) {
+            contentPerFile.put(file,
+                    new String(Files.readAllBytes(Paths.get(folder + File.separator + file))));
+        }
+        return contentPerFile.get(file);
     }
 }
--- src/com/bao/fileserver/inter/TextFileCache.java
+++
@@ -1,9 +0,0 @@
-package com.bao.fileserver.inter;
-
-public interface TextFileCache {
-    public void put(String fileName, String content);
-
-    public String get(String fileName);
-
-    public void setMaxCapacity(int maxCapacity);
-}
